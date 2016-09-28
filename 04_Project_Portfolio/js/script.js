784d39fac86e2e61b68b38c7c8eace280813346b
Viacheslav Dovnich <v.dovnich@gmail.com>
2016-09-28 15:31:52 +0200

    menu and p padding fixed

--- 04_Project_Portfolio/js/script.js
+++ 04_Project_Portfolio/js/script.js
@@ -27,36 +27,36 @@ var element6 = $("#Personal").offset().top;
 var element7 = $("#Recommend").offset().top;
 $(window).scroll(function() {
     var y = $(window).scrollTop();
-    if (y >= element1) {
+    if (y >= element1 -70) {
         document.getElementById("s1").style.display = "inline-block";
         document.getElementById("s2").style.display = "none";
     }
-    if (y >= element2) {
+    if (y >= element2 -70) {
         document.getElementById("s1").style.display = "none";
         document.getElementById("s2").style.display = "inline-block";
         document.getElementById("s3").style.display = "none";
     }
-    if (y >= element3) {
+    if (y >= element3 -70) {
         document.getElementById("s2").style.display = "none";
         document.getElementById("s3").style.display = "inline-block";
         document.getElementById("s4").style.display = "none";
     }
-    if (y >= element4) {
+    if (y >= element4 -70) {
         document.getElementById("s3").style.display = "none";
         document.getElementById("s4").style.display = "inline-block";
         document.getElementById("s5").style.display = "none";
     }
-    if (y >= element5) {
+    if (y >= element5 -70) {
         document.getElementById("s4").style.display = "none";
         document.getElementById("s5").style.display = "inline-block";
         document.getElementById("s6").style.display = "none";
     }
-    if (y >= element6) {
+    if (y >= element6 -70) {
         document.getElementById("s5").style.display = "none";
         document.getElementById("s6").style.display = "inline-block";
         document.getElementById("s7").style.display = "none";
     }
-    if (y >= element7) {
+    if (y >= element7 -70) {
         document.getElementById("s6").style.display = "none";
         document.getElementById("s7").style.display = "inline-block";
     }
