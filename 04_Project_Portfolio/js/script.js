0bb3b4ad978995ef900aa759683bc7036d9d2dac
Viacheslav Dovnich <v.dovnich@gmail.com>
2016-09-29 02:12:00 +0200

    Added content and certificates thumbnails block

--- 04_Project_Portfolio/js/script.js
+++ 04_Project_Portfolio/js/script.js
@@ -17,7 +17,7 @@ function reloadFunction() {
 }
 /* End */
 
-/* Current Position */
+/* Current Position in menu */
 var element1 = $("#About").offset().top;
 var element2 = $("#Professional").offset().top;
 var element3 = $("#Education").offset().top;
@@ -27,36 +27,36 @@ var element6 = $("#Personal").offset().top;
 var element7 = $("#Recommend").offset().top;
 $(window).scroll(function() {
     var y = $(window).scrollTop();
-    if (y >= element1 -70) {
+    if (y >= element1 - 70) {
         document.getElementById("s1").style.display = "inline-block";
         document.getElementById("s2").style.display = "none";
     }
-    if (y >= element2 -70) {
+    if (y >= element2 - 70) {
         document.getElementById("s1").style.display = "none";
         document.getElementById("s2").style.display = "inline-block";
         document.getElementById("s3").style.display = "none";
     }
-    if (y >= element3 -70) {
+    if (y >= element3 - 70) {
         document.getElementById("s2").style.display = "none";
         document.getElementById("s3").style.display = "inline-block";
         document.getElementById("s4").style.display = "none";
     }
-    if (y >= element4 -70) {
+    if (y >= element4 - 70) {
         document.getElementById("s3").style.display = "none";
         document.getElementById("s4").style.display = "inline-block";
         document.getElementById("s5").style.display = "none";
     }
-    if (y >= element5 -70) {
+    if (y >= element5 - 70) {
         document.getElementById("s4").style.display = "none";
         document.getElementById("s5").style.display = "inline-block";
         document.getElementById("s6").style.display = "none";
     }
-    if (y >= element6 -70) {
+    if (y >= element6 - 70) {
         document.getElementById("s5").style.display = "none";
         document.getElementById("s6").style.display = "inline-block";
         document.getElementById("s7").style.display = "none";
     }
-    if (y >= element7 -70) {
+    if (y >= element7 - 70) {
         document.getElementById("s6").style.display = "none";
         document.getElementById("s7").style.display = "inline-block";
     }
