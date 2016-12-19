2c1005efa648ff378332877b1909fcfdcf61ce50
Viacheslav Dovnich <v.dovnich@gmail.com>
2016-12-19 18:25:45 +0100

    New solution for random quote machine. Added this project to the main page.

--- Project_RandomQuoteMachine/script/mainJs.js
+++ Project_RandomQuoteMachine/script/mainJs.js
@@ -1,3 +1,6 @@
+//This variant does not work with https protocol
+//Start
+/*
 var post;
 
 function getQuote() {
@@ -5,11 +8,8 @@ function getQuote() {
         url: 'http://quotesondesign.com//wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1',
         success: function(data) {
             post = data.shift();
-
             $('#quote-content').html("<em>" + post.content + "</em>");
             $('#quote-title').text(' - ' + post.title);
-            
-
             if (typeof post.custom_meta !== 'undefined' && typeof post.custom_meta.Source !== 'undefined') {
                 $('#quote-source').html('Source:' + post.custom_meta.Source);
             } else {
@@ -21,9 +21,7 @@ function getQuote() {
 }
 
 function postTwitter() {
-   
     var plainText = post.content.slice(3, post.content.length - 5);
-
     if (post.content.length + post.title.length > 140) {
         var tempString = plainText.slice(0, 132 - post.title.length);
         window.open('https://twitter.com/intent/tweet?text=' + encodeURIComponent('"' + tempString + '...' + '"' + ' - ' + post.title));
@@ -35,7 +33,49 @@ function postTwitter() {
 $(document).ready(function() {
     getQuote();
     $('#getQuote').on('click', getQuote);
-
     $('#postTwitter').on('click', postTwitter);
-
 });
+*/
+//End
+
+//New solution
+//Start
+var responseQuote;
+var responseAuthor;
+
+function getQuote() {
+  $.ajax({
+    headers: {
+      "X-Mashape-Key": "2n5fJiMTdEmshqW92nSbpjJlnBijp1KT8McjsnFnBr26ikWBa6",
+      "Content-Type": "application/x-www-form-urlencoded",
+       Accept: "application/json"
+    },
+    url: 'https://andruxnet-random-famous-quotes.p.mashape.com/cat=',
+    success: function(response) {
+      var resp = JSON.parse(response);
+      responseQuote = '<em>"' + resp.quote + '"</em>';
+      responseAuthor = ' - ' + resp.author;
+        
+        $('#quote-content').html(responseQuote);
+            $('#quote-title').text(responseAuthor);
+        
+    }});
+}
+    
+function postTwitter() {
+    var plainText = responseQuote.slice(4, responseQuote.length - 5);
+    if (plainText.length + responseAuthor.length > 140) {
+        var tempString = plainText.slice(0, 137 - responseAuthor.length);
+        window.open('https://twitter.com/intent/tweet?text=' + encodeURIComponent(tempString + '...' + responseAuthor));
+    } else {
+        window.open('https://twitter.com/intent/tweet?text=' + encodeURIComponent(plainText + responseAuthor));
+    }
+}
+
+$(document).ready(function() {
+    getQuote();
+    $('#getQuote').on('click', getQuote);
+    $('#postTwitter').on('click', postTwitter);
+});
+    
+//End
